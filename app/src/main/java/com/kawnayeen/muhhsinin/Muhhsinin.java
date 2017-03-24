8afd622a9f6a1cdd62a2a835803f664d511cdcd9
kawnayeen <kawnayeen.08@gmail.com>
2017-03-24 17:00:12 +0600

    Initial commit

---
+++ app/src/main/java/com/kawnayeen/muhhsinin/Muhhsinin.java
@@ -0,0 +1,13 @@
+package com.kawnayeen.muhhsinin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Muhhsinin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_muhhsinin);
+    }
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+Muhhsinin - Android Application
+===============================
+
+This repository is for muhhsinin android application. 
+
+Contributor
+-----------
+Muhammad Kamarul Kawnayeen ([@kawnayeen](https://github.com/kawnayeen))
